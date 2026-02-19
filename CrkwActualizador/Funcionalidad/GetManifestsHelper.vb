1b504a8a9daccc1909882220eb3c8c5373618b93
croketworld <arecortb@gmail.com>
2026-02-19 19:00:52 +0100

    GC en GetManifestsHelper para que no se borre el AppUpdateManifest de la variable tras su establecimiento después de descargar y deserializar el manifiesto

--- CrkwActualizador/Funcionalidad/GetManifestsHelper.vb
+++ CrkwActualizador/Funcionalidad/GetManifestsHelper.vb
@@ -2,6 +2,7 @@
 Imports System.IO
 Imports System.Net
 Imports System.Net.Http.Headers
+Imports System.Runtime
 Imports System.Runtime.Serialization.Json
 
 Public Class GetManifestsHelper
@@ -9,6 +10,16 @@ Public Class GetManifestsHelper
     Private Shared tempfilePath As String
 
     Public Shared LastAppUpdateManifestDownloaded As AppUpdateManifest
+    ''' <summary>
+    ''' Develve el último manifiesto de actualización de aplicación y realiza una limpieza de memoria para quitarlo de la variable <see cref="LastAppUpdateManifestDownloaded"/>
+    ''' </summary>
+    ''' <returns></returns>
+    Public Shared Function GetLastAppUpdateManifestDownloaded() As AppUpdateManifest
+        Dim res As AppUpdateManifest = LastAppUpdateManifestDownloaded
+        GC.EndNoGCRegion()
+        GC.Collect(4)
+        Return res
+    End Function
 
     Public Shared Sub GetAppUpdateManifest(aplicacion As String, version As String)
         Dim url As String = CroketWorldConstants.GetUrlForAppUpdateManifest(aplicacion, version)
@@ -21,13 +32,18 @@ Public Class GetManifestsHelper
     End Sub
 
     Private Shared Sub GetAppUpdateManifest(url As Uri)
+        GC.Collect()
+
 #Disable Warning SYSLIB0014 ' El tipo o el miembro están obsoletos
         Using wc As New WebClient
 #Enable Warning SYSLIB0014 ' El tipo o el miembro están obsoletos
             tempfilePath = Path.GetTempFileName()
+            GC.KeepAlive(tempfilePath)
+            GC.TryStartNoGCRegion(16 * 1024 * 1024)
             LastAppUpdateManifestDownloaded = Nothing
             AddHandler wc.DownloadFileCompleted, AddressOf AppupdatemanifestDownloaded
             wc.DownloadFileAsync(url, tempfilePath)
+            GC.KeepAlive(tempfilePath)
         End Using
     End Sub
 
@@ -52,7 +68,9 @@ Public Class GetManifestsHelper
 
     Private Shared Sub AppupdatemanifestDownloaded(sender As Object, e As AsyncCompletedEventArgs)
         Dim res = DeserializeAppUpdateManfiest()
+        GC.KeepAlive(LastAppUpdateManifestDownloaded)
         LastAppUpdateManifestDownloaded = res
+        GC.KeepAlive(LastAppUpdateManifestDownloaded)
     End Sub
 
 
